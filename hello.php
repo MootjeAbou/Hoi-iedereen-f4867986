95137626136886e58c0ed8ad668b856dae049703
Mohammed <mohammed.aboubaqr@gmail.com>
2019-11-28 12:22:28 +0100

    bruh

--- hello.php
+++ hello.php
@@ -1,15 +1,2 @@
 <?php
 echo "Hello world";
-
-echo "Last statement";
-
-echo 'This ', 'string ', 'was ', 'made ', 'with multiple parameters.', chr(10);
-
-echo 'This ' . 'string ' . 'was ' . 'made ' . 'with concatenation.' . "\n";
-
-($some_var) ? echo 'true' : echo 'false';
-
-($some_var) ? print 'true' : print 'false';
-
-echo $some_var ? 'true': 'false';
-?>
